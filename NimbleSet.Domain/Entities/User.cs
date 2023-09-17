c419ee10129d8259989aac6c323633f1c7ffb57a
nimagap <ikkimingolti@.com>
2023-09-17 21:43:24 +0500

    commit new

--- NimbleSet.Domain/Entities/User.cs
+++ NimbleSet.Domain/Entities/User.cs
@@ -14,7 +14,6 @@ namespace Domain.Entities
         public string Email { get; set; }
         public string Password { get; set; }
         public string PhoneNumber { get; set; }
-        public Roll Rolls { get; set; }
-
+        public Roll roll { get; set; }
     }
 }
