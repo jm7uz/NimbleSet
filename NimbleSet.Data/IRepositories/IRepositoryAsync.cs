b8a200cd4838fff8ce7a96a43641e193168b2dfd
Jaloliddin | Jm7Uz <87977243+jm7uz@users.noreply.github.com>
2023-09-17 22:41:35 +0500

    new commit v1.1

---
+++ NimbleSet.Data/IRepositories/IRepositoryAsync.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.IRepositories
+{
+    public interface IRepositoryAsync<TEntity>
+    {
+        public Task<TEntity> InsertAsync(TEntity entity);
+        public Task<TEntity> UpdateAsync(TEntity entity);
+        public Task<bool> DeleteAsync(long Id);
+        public Task<TEntity> SelecttByIdAsync(long Id);
+        public Task<List<TEntity>> SelectAllAsync();
+
+    }
+}
---
+++ NimbleSet.Presentation/UI/WelcomePage.cs
@@ -0,0 +1,57 @@
+﻿
+namespace NimbleSet.Presentation.UI;
+
+public class WelcomePage
+{
+    public class CaptchaResult
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+        public int CorrectSum { get; set; }
+    }
+    public CaptchaResult CaptchaGenerator()
+    {
+        Random random = new Random();
+        int number1 = random.Next(1, 101);
+        int number2 = random.Next(1, 101);
+
+        int correctSum = number1 + number2;
+
+        CaptchaResult result = new CaptchaResult
+        {
+            Number1 = number1,
+            Number2 = number2,
+            CorrectSum = correctSum
+        };
+
+        return result;
+    }
+
+    public bool Validator(int a, int b, int num)
+    {
+        int result = a + b;
+        return result == num;
+
+    }
+
+    public bool Signin()
+    {
+        try
+        {
+            CaptchaResult captchaValidCheck = CaptchaGenerator();
+            Console.Write($"{captchaValidCheck.Number1} + {captchaValidCheck.Number2} = ?\nInput correct result: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (Validator(captchaValidCheck.Number1, captchaValidCheck.Number2, number))
+                return true;
+            return false;
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Unexcepted Error");
+            return false;
+        }
+    }
+
+}
