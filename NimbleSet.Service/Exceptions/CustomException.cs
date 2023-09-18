0a4ffa5043594e37b0baaa1bf8c9274918ffa909
Avazbek Abdumajidov <abdumajidova51@gmail.com>
2023-09-18 15:57:14 +0500

    Added Exception

---
+++ NimbleSet.Service/Exceptions/CustomException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimbleSet.Service.Exceptions
+{
+    public class CustomException : Exception
+    {
+        public int StatusCode { get; set; }
+
+        public CustomException(int code, string message) : base(message)
+        {
+            this.StatusCode = code;
+        }
+    }
+}
