a7837bf7c918434a0a69750efc92ad0350358153
Avazbek Abdumajidov <abdumajidova51@gmail.com>
2023-09-18 15:38:24 +0500

    Full Dtos and Interfaces

---
+++ NimbleSet.Service/Interfaces/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using Services.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        public Task<bool> DeleteAsync(long id);
+        public Task<List<CategoryForRezultDto>> GetAllAsync();
+        public Task<CategoryForRezultDto> GetByIdAsync(long id);
+        public Task<CategoryForRezultDto> UpdateAsync(CategoryForUpdateDto user);
+        public Task<CategoryForRezultDto> InsertAsync(CategoryForCreationDto categoryDto);
+    }
+}
